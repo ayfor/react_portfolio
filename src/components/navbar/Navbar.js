7a2fd4c13d9a24a3385ecd20bda3b7db81d3c455
ayfor <josh.a.stubbington@gmail.com>
2021-07-26 19:00:38 -0400

    added picture to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,13 @@
+import picture from '../../assets/images/selfie.jpg';
+import './Navbar.css'
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid container">
-        <a className="navbar-brand" href="#">Joshua Stubbington</a>
+        
+        <a className="navbar-brand" href="#"><img id="display-pic" src={picture}/>Joshua Stubbington</a>
+        
         <div className= "d-flex ms-2">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,10 +15,10 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">About Me</a>
+                  <a className="nav-link active" aria-current="page" href="/">About Me</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Projects</a>
+                  <a className="nav-link" href="/projects">Projects</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Contact</a>
