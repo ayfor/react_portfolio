1e724451346b64a38bf2c61f49d7b09c2a366956
ayfor <josh.a.stubbington@gmail.com>
2021-07-26 22:20:50 -0400

    added projects, contact form, renamed navbar to Header and boilerplate for resume list

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Navbar from './navbar/Navbar'
+import Header from './header/Header'
 import About from './about/About'
 import Projects from './projects/Projects';
+import ContactForm from './contact/Contact';
+import Resume from './resume/Resume';
 
 import './App.css';
 
 function App() {
     return (
     <Router>
-        <Navbar /> 
+        <Header /> 
         <Switch>
             <Route exact path="/">
                 <About/>
             </Route>
             <Route path="/projects">
                 <Projects/>
+            </Route>
+            <Route path="/contact">
+                <ContactForm/>
+            </Route>
+            <Route path="/resume">
+                <Resume/>
             </Route>
         </Switch>
     </Router>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -25,6 +25,19 @@ let projectsArray = [
         link:"https://ayfor.github.io/left-over-variables/",
         img:"leftovervars_screenshot.png"
     }, 
+    {
+        title:"Weather Dashboard",
+        description:"This small application enables users to search for a city in the input field and recieve weather data for the city. Once a city is searched, the city is added to a card to the left hand side of the page. A user can then click on that card to revisit the weather information for that city.",
+        link:"https://github.com/ayfor/weather_dashboard",
+        img:"weather_dashboard_screenshot.png"
+    }, 
+    {
+        title:"Work Day Scheduler",
+        description:"This application enables a user to enter tasks for each hour of a given day with the current date listed at the top of the page. All entered tasks are stored in local storage so a user may navigate away from the page and revisit their tasks later on.",
+        link:"https://github.com/ayfor/day_scheduler",
+        img:"day_scheduler_screenshot.png"
+    }, 
+
 ]
 
 const Projects = () => {
